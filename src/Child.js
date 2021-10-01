73c465b04231dfa0848cbb997b9cb34fd8741ae9
Omar <omar.branez@gmail.com>
2021-10-01 12:54:36 -0400

    commit tree 49ea120aaaa2ff231ab2b32384d28d23a004721e

--- src/Child.js
+++ src/Child.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 import { getRandomColor } from './randomColorGenerator.js'
 
+// all i do is receive the color from the parent
 class Child extends Component {
   render() {
+    console.log(this.props)
     return (
-      <div
+      <div onClick={()=>this.props.handleColorChange(getRandomColor())} // now that the function takes in an argument, must be a function that invokes handlecolorchange
         className="child"
-        style={{backgroundColor: "#FFF"}}
+        style={{backgroundColor: this.props.color}}
       ></div>
     )
   }
